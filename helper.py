ecde5b046659f358862660946baec0f838fccc38
erick <erick_093@hotmail.com>
2020-05-23 21:48:30 +0200

    adding new information to the synthetic data classifiers

--- helper.py
+++ helper.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
 #function from  https://www.kaggle.com/rpsuraj/outlier-detection-techniques-simplified/notebook
 def out_iqr(df , column):
     global lower,upper
@@ -36,3 +40,161 @@ def out_zscore(data):
             outliers.append(i)
     print("Total number of outliers are", len(outliers))
     return  outliers,zscore
+
+def plot_learning_curves(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5),name=None):
+
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt,test_scores_mean
+
+
+def plt_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                       n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Testing score")
+
+    plt.legend(loc="best")
+    return plt, test_scores_mean
+
+def plot_datasets(datasets,classifiers,names):
+    figure = plt.figure(figsize=(27, 27))
+    i=1
+    j=1
+    h = .02
+    for ds_cnt, ds in enumerate(datasets):
+        X, y = ds
+        X = StandardScaler().fit_transform(X)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=.4, random_state=42)
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+        #cm = plt.cm.RdBu
+        cm = plt.cm.PuOr
+        #cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+        cm_bright = ListedColormap(['#FF5733', '#6C3483'])
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        if ds_cnt == 0:
+            ax.set_title("Input data",fontsize=30)
+        ax.text(xx.max() - .3, yy.min() + .3, "Dataset {}".format(j),
+                                    size=25, horizontalalignment='right', fontweight='bold')
+
+        ax.scatter(X_train[:, 0], X_train[:, 1],s=50 , c=y_train, cmap=cm_bright,
+                   edgecolors='k')
+
+        ax.scatter(X_test[:, 0], X_test[:, 1],s=50 , c=y_test, cmap=cm_bright, alpha=0.4,
+                   edgecolors='k')
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        i += 1
+
+
+        for name, clf in zip(names, classifiers):
+            ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+            clf.fit(X_train, y_train)
+            score_test = clf.score(X_test, y_test)
+            score_train = clf.score(X_train, y_train)
+
+            if hasattr(clf, "decision_function"):
+                Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+            else:
+                Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+
+            Z = Z.reshape(xx.shape)
+            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+
+            ax.scatter(X_train[:, 0], X_train[:, 1],s=50 ,c=y_train, cmap=cm_bright,
+                       edgecolors='k')
+
+            ax.scatter(X_test[:, 0], X_test[:, 1], s=50, c=y_test, cmap=cm_bright,
+                       edgecolors='k', alpha=0.4)
+
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+            if ds_cnt == 0:
+                ax.set_title(name,fontsize=30)
+            ax.text(xx.max() - .3, yy.min() + .3, 'Test_score: '+('%.2f' % score_test).lstrip('0') + '  Train_score: '+('%.2f' % score_train).lstrip('0'),
+                    size=25, horizontalalignment='right', fontweight='bold')
+            i += 1
+        j += 1
+    plt.tight_layout()
+    plt.show()
